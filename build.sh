460a4f6f8547de23fc7cbda4f0b480ddd48e3b2c
simonwinter <simon@dragonfly.co.nz>
2020-08-06 15:51:41 +1200

    gorby update

--- build.sh
+++ build.sh
@@ -5,21 +5,18 @@ set -euo pipefail
 
 export RUN=
 
-(cd site && make install)
-(cd site && make clean)
-(cd site && make build)
-(cd site && make compress)
+make install
 
-cp static-site.tgz /output
-cp -rf public/* /publish/
+# cp static-site.tgz /output
+# cp -rf public/* /publish/
 
-s3cmd --acl-public \
-      -v \
-      --no-mime-magic \
-      --guess-mime-type \
-      --no-preserve \
-      --delete-removed \
-      --access_key=${AWS_ACCESS_KEY_ID} \
-      --secret_key=${AWS_SECRET_ACCESS_KEY} \
-      --cf-invalidate \
-      sync -r /publish/* s3://${AWS_BUCKET}/
+# s3cmd --acl-public \
+#       -v \
+#       --no-mime-magic \
+#       --guess-mime-type \
+#       --no-preserve \
+#       --delete-removed \
+#       --access_key=${AWS_ACCESS_KEY_ID} \
+#       --secret_key=${AWS_SECRET_ACCESS_KEY} \
+#       --cf-invalidate \
+#       sync -r /publish/* s3://${AWS_BUCKET}/
